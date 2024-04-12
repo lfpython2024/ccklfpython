0562c3d1a0eec462570fbe4b6b7a6e60d308c877
lfpython2024 <166484308+lfpython2024@users.noreply.github.com>
2024-04-12 09:26:25 +0800

    Update 识别.py

--- pages/识别.py
+++ pages/识别.py
@@ -9,18 +9,17 @@ def image_to_words(image_path):
     return words
 
 def save_words(save_path, words):
-    f = open(save_path, 'w')
+    f = open(save_path, 'w', encoding='utf-8')  # 指定编码方式为 UTF-8
     f.write(words)
     f.close()
 
 # 输入图像路径的交互组件
-image_path = st.text_input("请输入图像路径：")
-
+image_path = st.text_input("请输入图像路径：", encoding='utf-8')  # 指定编码方式为 UTF-8
 # 输入保存路径的交互组件
-save_path = st.text_input("请输入保存路径：")
+save_path = st.text_input("请输入保存路径：", encoding='utf-8')  # 指定编码方式为 UTF-8
 
 if st.button("转换并保存"):
     words = image_to_words(image_path)
     st.write(words)  # 在这里添加直接显示识别的结果
-
-st.write("转换完成！")
+    save_words(save_path, words)
+    st.write("转换完成！")
